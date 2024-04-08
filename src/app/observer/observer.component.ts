2215163403157983f3407cbae15b9690bede9ee7
Miru <hungche0701@gmail.com>
2024-04-09 03:33:04 +0800

    feat:修改observer

--- src/app/observer/observer.component.ts
+++ src/app/observer/observer.component.ts
@@ -1,68 +1,54 @@
-import { Component } from '@angular/core';
-
-// 觀察者 A
-class ObserverA {
-  notify(id: number): void {
-    console.log(`我是觀察者 A，我收到影片 ${id} 上架通知了`);
-  }
-}
-
-// 觀察者 B
-class ObserverB {
-  notify(id: number): void {
-    console.log(`我是觀察者 B，我收到影片 ${id} 上架通知了`);
-  }
-}
+import { Component, OnDestroy } from '@angular/core';
+import { Subject, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-observer',
   templateUrl: './observer.component.html',
-  styles: [
-  ]
+  styles: []
 })
-export class ObserverComponent {
-  observers: any[] = [];
+export class ObserverComponent implements OnDestroy {
+  private youtuber$ = new Subject<number>();
+  private observerASubscription: Subscription;
+  private observerBSubscription: Subscription;
 
   constructor() {
     console.log('--- 開始 ---');
 
     // 影片 1 上架，此時還沒有觀察者
-    this.notifyObservers(1);
+    this.youtuber$.next(1);
 
-    // 加入觀察者 A，也就是觀察者 A 開啟通知了
-    this.addObserver(new ObserverA());
+    // 建立觀察者 A 物件
+    const observerA = {
+      next: (id: number) => {
+        console.log(`我是觀察者 A，我收到影片 ${id} 上架通知了`);
+      }
+    };
+    // 加入觀察者 A
+    this.observerASubscription = this.youtuber$.subscribe(observerA);
+
     // 影片 2 上架，此時觀察者 A 會收到通知
-    this.notifyObservers(2);
+    this.youtuber$.next(2);
 
-    // 加入觀察者 B，也就是觀察者 B 開啟通知了
-    this.addObserver(new ObserverB());
+    // 加入觀察者 B
+    this.observerBSubscription = this.youtuber$.subscribe((id: number) => {
+      console.log(`我是觀察者 B，我收到影片 ${id} 上架通知了`);
+    });
+
     // 影片 3 上架，此時觀察者 A 跟 B 都會收到通知
-    this.notifyObservers(3);
+    this.youtuber$.next(3);
 
-    // 移除觀察者 B，也就是觀察者 B 關閉通知了
-    this.deleteObserver(new ObserverB());
+    // 移除觀察者 B
+    // 在 ReactiveX 中也稱為「取消訂閱」
+    this.observerBSubscription.unsubscribe();
+
     // 影片 4 上架，此時只剩下觀察者 A 會收到通知
-    this.notifyObservers(4);
+    this.youtuber$.next(4);
 
     console.log('--- 結束 ---');
   }
 
-  // 通知所有觀察者新影片上架了
-  notifyObservers(id: number): void {
-    // 列舉出每個觀察者，並進行通知動作
-    this.observers.forEach(observer => {
-      observer.notify(id);
-    });
+  ngOnDestroy(): void {
+    // 取消訂閱以避免內存洩漏
+    this.observerASubscription.unsubscribe();
   }
-
-  // 加入新的觀察者，也就是有新使用者開啟通知了
-  addObserver(observer: any): void {
-    this.observers.push(observer);
-  }
-
-  // 將某個觀察者移除，也就是某個使用者關閉通知了
-  deleteObserver(observerToRemove: any): void {
-    this.observers = this.observers.filter(observer => observer.constructor !== observerToRemove.constructor);
-  }
-
 }
